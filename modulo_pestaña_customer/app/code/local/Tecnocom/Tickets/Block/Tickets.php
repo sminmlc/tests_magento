71525249395cd2cbda67b28304296f69d8123cab
Marc López Cobero <m.1991.lp@gmail.com>
2017-08-08 14:51:55 +0200

    Update Tickets.php

--- modulo_pestaña_customer/app/code/local/Tecnocom/Tickets/Block/Tickets.php
+++ modulo_pestaña_customer/app/code/local/Tecnocom/Tickets/Block/Tickets.php
@@ -5,14 +5,14 @@
  * PHP version 5
  *
  * @codepool Local
- * @category Tecnocom
- * @package Tecnocom_Tickets
+ * @category Sminmlc
+ * @package Sminmlc_Tickets
  * @module Tickets
- * @copyright 2016 Tecnocom/Aether
+ * @copyright 2016 Sminmlc
  * @license GNU General Public License v3 <http://www.gnu.org/licenses/gpl-3.0.txt>
  * @version SVN:
  */
-class Tecnocom_Tickets_Block_Tickets extends Mage_Core_Block_Template
+class Sminmlc_Tickets_Block_Tickets extends Mage_Core_Block_Template
 {
 	public function myfunction()
     {
