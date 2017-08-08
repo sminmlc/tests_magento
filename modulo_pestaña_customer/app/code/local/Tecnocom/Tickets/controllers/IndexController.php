f36348df353fded633e94b286b3930d9fa56c9aa
Marc López Cobero <m.1991.lp@gmail.com>
2017-08-08 14:50:32 +0200

    Update IndexController.php

--- modulo_pestaña_customer/app/code/local/Tecnocom/Tickets/controllers/IndexController.php
+++ modulo_pestaña_customer/app/code/local/Tecnocom/Tickets/controllers/IndexController.php
@@ -5,14 +5,14 @@
  * PHP version 5
  *
  * @codepool Local
- * @category Tecnocom
- * @package Tecnocom_Tickets
+ * @category Sminmlc
+ * @package Sminmlc_Tickets
  * @module Tickets
- * @copyright 2016 Tecnocom/Aether
+ * @copyright 2016 Sminmlc
  * @license GNU General Public License v3 <http://www.gnu.org/licenses/gpl-3.0.txt>
  * @version SVN:
  */
-class Tecnocom_Tickets_IndexController extends Mage_Core_Controller_Front_Action
+class Sminmlc_Tickets_IndexController extends Mage_Core_Controller_Front_Action
 {
 	public function indexAction(){
 		$this->loadLayout();
