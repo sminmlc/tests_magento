310e39ce9bbb8ebb85ae771ec177fd635e4ebf8a
Marc López Cobero <m.1991.lp@gmail.com>
2017-08-08 14:41:47 +0200

    Update Webservices.php

--- modulo_grid_form_colores/app/code/local/Tecnocom/Webservices/Model/Resource/Webservices.php
+++ modulo_grid_form_colores/app/code/local/Tecnocom/Webservices/Model/Resource/Webservices.php
@@ -6,10 +6,10 @@
  * and open the template in the editor.
  */
 
-class Tecnocom_Webservices_Model_Resource_Webservices extends Mage_Core_Model_Mysql4_Abstract
+class Sminmlc_Webservices_Model_Resource_Webservices extends Mage_Core_Model_Mysql4_Abstract
 {
     public function _construct()
     {  
-        $this->_init('tecnocom_webservices/webservices', 'id');
+        $this->_init('sminmlc_webservices/webservices', 'id');
     }
 } 
