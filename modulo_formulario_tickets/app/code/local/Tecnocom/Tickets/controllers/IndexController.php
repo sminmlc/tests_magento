9595c6b473ebc8185ef16dc213b81ff88eb053dd
Marc López Cobero <m.1991.lp@gmail.com>
2017-08-08 14:22:06 +0200

    Update IndexController.php

--- modulo_formulario_tickets/app/code/local/Tecnocom/Tickets/controllers/IndexController.php
+++ modulo_formulario_tickets/app/code/local/Tecnocom/Tickets/controllers/IndexController.php
@@ -5,10 +5,10 @@
  * PHP version 5
  *
  * @codepool Local
- * @category Tecnocom
- * @package Tecnocom_Tickets
+ * @category Sminmlc
+ * @package Sminmlc_Tickets
  * @module Tickets
- * @copyright 2016 Tecnocom/Aether
+ * @copyright 2016 Sminmlc
  * @license GNU General Public License v3 <http://www.gnu.org/licenses/gpl-3.0.txt>
  * @version SVN:
  */
