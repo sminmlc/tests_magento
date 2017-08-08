bf777f8ed7beec5227b16f103d1b0f2c7286b10b
Marc López Cobero <m.1991.lp@gmail.com>
2017-08-08 14:42:42 +0200

    Update Data.php

--- modulo_grid_form_colores/app/code/local/Tecnocom/Webservices/Helper/Data.php
+++ modulo_grid_form_colores/app/code/local/Tecnocom/Webservices/Helper/Data.php
@@ -5,14 +5,14 @@
  * PHP version 5
  *
  * @codepool Local
- * @category Tecnocom
- * @package Tecnocom_Webservices
+ * @category Sminmlc
+ * @package Sminmlc_Webservices
  * @module Webservices
- * @copyright 2016 Tecnocom/Aether
+ * @copyright 2016 Sminmlc
  * @license GNU General Public License v3 <http://www.gnu.org/licenses/gpl-3.0.txt>
  * @version SVN:
  */
-class Tecnocom_Webservices_Helper_Data extends Mage_Core_Helper_Abstract
+class Sminmlc_Webservices_Helper_Data extends Mage_Core_Helper_Abstract
 {
     
 }
