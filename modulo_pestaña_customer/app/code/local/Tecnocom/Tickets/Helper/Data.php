b98a6c26ef0b6c21510533c9c0778a9aabe07b25
Marc López Cobero <m.1991.lp@gmail.com>
2017-08-08 14:51:25 +0200

    Update Data.php

--- modulo_pestaña_customer/app/code/local/Tecnocom/Tickets/Helper/Data.php
+++ modulo_pestaña_customer/app/code/local/Tecnocom/Tickets/Helper/Data.php
@@ -5,14 +5,14 @@
  * PHP version 5
  *
  * @codepool Local
- * @category Tecnocom
- * @package Tecnocom_Tickets
+ * @category Sminmlc
+ * @package Sminmlc_Tickets
  * @module Tickets
- * @copyright 2016 Tecnocom/Aether
+ * @copyright 2016 Sminmlc
  * @license GNU General Public License v3 <http://www.gnu.org/licenses/gpl-3.0.txt>
  * @version SVN:
  */
-class Tecnocom_Tickets_Helper_Data extends Mage_Core_Helper_Abstract
+class Sminmlc_Tickets_Helper_Data extends Mage_Core_Helper_Abstract
 {
     
 }
